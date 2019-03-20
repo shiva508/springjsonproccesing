82db66b00358ff941f3c0e43dd0f2e4001fc5c8a
shiva <shiva@pgi-amarendrad1.Prodigy.local>
2019-03-20 17:31:00 +0530

    Shit head

--- src/main/java/com/demo/service/EmployeeServiceImpl.java
+++ src/main/java/com/demo/service/EmployeeServiceImpl.java
@@ -9,15 +9,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class EmployeeServiceImpl {
-public void conVersions() {
-	Employee employee=new Employee("Shiva", "Development");
-}
+	ObjectMapper om;
 
-public String toJson(Employee employee) throws IOException{
-	ObjectMapper om=new ObjectMapper();
-	return om.writeValueAsString(employee);
-}
-public Employee toEmployee(String jsonData) throws IOException{ 
-	return null;
-}
+	public EmployeeServiceImpl() {
+		om = new ObjectMapper();
+	}
+
+	public void conVersions() {
+		Employee employee = new Employee("Shiva", "Development");
+	}
+
+	public String toJson(Employee employee) throws IOException {
+		return om.writeValueAsString(employee);
+	}
+
+	public Employee toEmployee(String jsonData) throws IOException {
+		return om.readValue(jsonData, Employee.class);
+	}
 }
